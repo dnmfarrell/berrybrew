5daa87d1e1c4e117d49aaf59395648506c1281e0
sillymoose <davidnmfarrell@gmail.com>
2014-10-08 20:04:33 -0400

    initial version

---
+++ src/berrybrew.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Berrybrew
+{
+    public class Berrybrew
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        internal static void Fetch ()
+        {
+
+        }
+
+        internal static void Extract ()
+        {
+
+        }
+
+        internal static void Install ()
+        {
+
+        }
+
+        internal static void Switch ()
+        {
+
+        }
+
+        internal static void Help ()
+        {
+
+        }
+
+        internal static void Available ()
+        {
+
+        }
+    }
+}
