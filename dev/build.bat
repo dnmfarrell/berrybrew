becc8810adaebbcf56f02abe0c239c9ba91bf52d
PeterCJ <pryrtcode@pryrt.com>
2017-07-07 12:36:55 -0700

    rework build.bat to populate build\data, to use ICSharpCode rather than Ionic

--- dev/build.bat
+++ dev/build.bat
@@ -1,5 +1,7 @@
+@echo off
 mkdir build
 mkdir build\data
+copy dev\data\*.json build\data                 & rem [pryrt] for Contributing.md::`build\berrybrew <cmd> [opts]` to work, build\data\config.json _must_ exist
 
 echo "compiling dll..."
 
@@ -13,11 +15,12 @@ call mcs^
 echo "compiling binary..."
 
 call mcs^
-    src\bbconsole.cs^
     -lib:build^
     -r:bbapi.dll^
     -win32icon:inc/berrybrew.ico^
-    -out:build/berrybrew.exe
+    -out:build/berrybrew.exe^
+    src\bbconsole.cs
 
-copy bin\Ionic.Zip.dll build\
+rem copy bin\Ionic.Zip.dll build\               & rem [pryrt] Ionic* no longer exists
+copy bin\ICSharpCode.SharpZipLib.dll build\     & rem [pryrt] but ICSharpCode* _is_ needed
 copy bin\Newtonsoft.Json.dll build\
