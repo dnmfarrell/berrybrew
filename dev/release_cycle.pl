9b7bc2d3c470ea7057a3da97142de4bc995ad94d
Steve Bertrand <steveb@cpan.org>
2023-07-22 18:54:29 -0300

    Updated stderro check

--- dev/release_cycle.pl
+++ dev/release_cycle.pl
@@ -41,7 +41,7 @@ sub checkout_master_branch {
     $trap->unhook;
 
     print ">$_<\n" for $trap->stderr;
-    if ($output !~ /Switched to branch 'master'/) {
+    if (! grep { $_ =~ /Switched to branch 'master'/ } $trap->stderr) {
         warn "Couldn't switch to master branch";
     }
 }
