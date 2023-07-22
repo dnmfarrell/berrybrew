93f128908b68b2d0df721995c39f66c75500ebd1
Steve Bertrand <steveb@cpan.org>
2023-07-22 18:56:51 -0300

    Debugging

--- dev/release_cycle.pl
+++ dev/release_cycle.pl
@@ -40,7 +40,7 @@ sub checkout_master_branch {
     `git checkout master`;
     $trap->unhook;
 
-#    print Dumper $trap->stderr;
+    print Dumper $trap->stderr;
 
 #    if (! grep { $_ =~ /Switched to branch 'master'/ } $trap->stderr) {
 #        warn "Couldn't switch to master branch";
